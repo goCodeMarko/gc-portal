cbdcfa079e06fc2e81fc06560629994e96af4612
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-03-13 12:38:54 +0800

    Added web camera

--- src/app/frontliner/frontliner.module.ts
+++ src/app/frontliner/frontliner.module.ts
@@ -6,9 +6,15 @@ import { RouterModule } from "@angular/router";
 import { FrontlinerRoutes } from "./frontliner-routing";
 import { CashInComponent } from "./cash-in/cash-in.component";
 import { FormsModule } from "@angular/forms";
+import { WebcamModule } from "ngx-webcam";
 
 @NgModule({
   declarations: [FrontlinerComponent, CashOutComponent, CashInComponent],
-  imports: [CommonModule, FormsModule, RouterModule.forChild(FrontlinerRoutes)],
+  imports: [
+    CommonModule,
+    FormsModule,
+    WebcamModule,
+    RouterModule.forChild(FrontlinerRoutes),
+  ],
 })
 export class FrontlinerModule {}

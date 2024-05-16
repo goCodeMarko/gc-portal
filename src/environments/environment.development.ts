26aefc8fec2bbc38bbbda0df6cfdf39d27221c91
Patrick Marck Dulaca <39939290+goCodeMarko@users.noreply.github.com>
2024-05-16 11:47:04 +0800

    Delete src/environments/environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  SERVER_URL: "https://gcportal-api-uat-856f4a8bfa2f.herokuapp.com/api/",
-  WEBSOCKET: "https://gcportal-api-uat-856f4a8bfa2f.herokuapp.com",
-};

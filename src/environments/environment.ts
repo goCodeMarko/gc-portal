271de723e4819cd13f4fc41b8b8648b33f69d8b3
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-05-17 14:48:09 +0800

    simple change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   env: "dev",
   // SERVER_URL: "http://localhost/api/",
   // SERVER_URL_81: "http://localhost:81/api/",
-  // WEBSOCKET: "http://localhost:81",
+  // WEBSOCKET: 'http://localhost:81"',
   SERVER_URL: "http://localhost:3001/api/",
   WEBSOCKET: "http://localhost:3001",
 };

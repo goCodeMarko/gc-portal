5ed0bb1263800d2e633a5f39e162f1c908b5fc7b
Patrick Marck Dulaca <39939290+goCodeMarko@users.noreply.github.com>
2024-05-18 23:17:03 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   env: "uat",
-  SERVER_URL: "https://gcportal-api-uat-856f4a8bfa2f.herokuapp.com/api/",
-  WEBSOCKET: "https://gcportal-api-uat-856f4a8bfa2f.herokuapp.com",
+  SERVER_URL: "https://159.223.78.213:3001/api/",
+  WEBSOCKET: "https://159.223.78.213:3001",
 };

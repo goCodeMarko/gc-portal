dc56d0a4de93989c7dcbf2816ea1d64def36446a
Patrick Marck Dulaca <39939290+goCodeMarko@users.noreply.github.com>
2024-05-18 23:21:22 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   env: "uat",
-  SERVER_URL: "https://159.223.78.213:3001/api/",
-  WEBSOCKET: "https://159.223.78.213:3001",
+  SERVER_URL: "http://159.223.78.213:3001/api/",
+  WEBSOCKET: "http://159.223.78.213:3001",
 };

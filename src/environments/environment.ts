b7202e74c5daffd703974a9910757fb58bb06142
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-05-21 01:01:26 +0800

    setup prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   env: "prod",
   SERVER_URL_444: "https://api.gcportal.space:444/api/",
   SERVER_URL: "https://api.gcportal.space/api/",

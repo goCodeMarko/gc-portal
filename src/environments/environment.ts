883ed4e87d3889b538e679f1d2e3f0c3413b6da6
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-05-25 08:52:48 +0800

    fixed socket issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  production: false,
-  env: "uat",
-  SERVER_URL: "http://159.223.78.213:3001/api/",
-  WEBSOCKET: "http://159.223.78.213:3001",
+  production: true,
+  env: "prod",
+  SERVER_URL_444: "https://api.gcportal.space:444/api/",
+  SERVER_URL: "https://api.gcportal.space/api/",
+  WEBSOCKET: "https://api.gcportal.space:444",
 };

be9b9645928532741921a5397ad08ba826f9d0c9
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-05-25 08:41:35 +0800

    update environemnt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,7 @@
 export const environment = {
-  production: false,
-  env: "dev",
-  // SERVER_URL: "http://localhost/api/",
-  // SERVER_URL_81: "http://localhost:81/api/",
-  // WEBSOCKET: 'http://localhost:81"',
-  SERVER_URL: "http://localhost:3001/api/",
-  WEBSOCKET: "http://localhost:3001",
+  production: true,
+  env: "prod",
+  SERVER_URL_444: "https://api.gcportal.space:444/api/",
+  SERVER_URL: "https://api.gcportal.space/api/",
+  WEBSOCKET: "https://api.gcportal.space:444",
 };

35d1ebacbbc92a741df24b14e97a0046f927e7f2
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-06-09 22:41:08 +0800

    fixing routes issue

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,23 +4,24 @@ import { SecurityGuard } from "./guards/security.guard";
 import { LoginComponent } from "./login/login.component";
 
 export const AppRoutes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" }, // Redirect root URL to login
   {
     path: "login",
     component: LoginComponent,
   },
   {
-    path: "",
+    path: "transaction",
     component: FullComponent,
     children: [
       {
-        path: "transaction",
+        path: "",
         loadChildren: () =>
           import("./transaction/transaction.module").then(
             (m) => m.TransactionModule
           ),
         canActivate: [SecurityGuard],
       },
-      { path: "**", redirectTo: "/login", pathMatch: "full" },
     ],
   },
+  { path: "**", redirectTo: "login", pathMatch: "full" }, // Wildcard route for unknown routes
 ];

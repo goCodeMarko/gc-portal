c183f3b06bdf5777b1b0c1a6bb8be8408c48179b
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-06-09 12:03:07 +0800

    Updates the routes and pages of the application

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -5,6 +5,7 @@ import * as moment from "moment";
 import { HttpRequestService } from "../http-request/http-request.service";
 import { MatDialog } from "@angular/material/dialog";
 import { PopUpModalComponent } from "../modals/pop-up-modal/pop-up-modal.component";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-transaction",
@@ -18,26 +19,22 @@ export class TransactionComponent implements OnInit {
   public tabActiveCashout = true;
   public tabActiveCashin = false;
   private viewTypeBefore: string = "";
-
-  //Cash in form
-  public transactionForm: FormGroup;
+  selectedRoutes: string = "cashout";
   public role: string = "";
   constructor(
     private auth: AuthService,
     private fb: FormBuilder,
     private hrs: HttpRequestService,
-    private dialog: MatDialog
-  ) {
-    this.transactionForm = this.fb.group({
-      date: ["", Validators.required],
-      gcash: ["", Validators.required],
-      cash_on_hand: ["", Validators.required],
-      gcashNumber: ["", [Validators.required, Validators.pattern(/^09\d{9}$/)]],
-    });
-  }
+    private dialog: MatDialog,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.checkRole();
+  }
+
+  selectOption(option: string) {
+    this.selectedRoutes = option;
   }
 
   async checkRole() {
@@ -45,90 +42,22 @@ export class TransactionComponent implements OnInit {
     this.role = user.role;
   }
 
-  view(type: string) {
-    if (type === "cashin") {
-      this.viewType = "cashin";
-      this.tabActiveCashin = true;
-      this.tabActiveCashout = false;
-      this.hideMainButton = false;
-    } else if (type === "cashout") {
-      this.viewType = "cashout";
-      console.log(this.viewType);
-      this.tabActiveCashin = false;
-      this.tabActiveCashout = true;
-      this.hideMainButton = false;
-    }
+  createTransactionPage() {
+    this.hideMainButton = true;
+    this.router.navigate(["/transaction/create"]);
   }
 
-  mainButton(event: boolean) {
-    this.hideMainButton = event;
+  fromRouterOutlet(component: any) {
+    if (component.hideMainButton) {
+      this.hideMainButton = false;
+      component.hideMainButton.subscribe((value: boolean) => {
+        this.hideMainButton = value;
+      });
+    }
   }
 
   public logout() {
     this.auth.logout();
-  }
-
-  public createTransactionView() {
-    this.hideMainButton = true;
-    this.viewTypeBefore = this.viewType;
-    this.viewType = "createTransaction";
-    this.transactionForm.reset();
-  }
-
-  public createTransBtnOnLoad = false;
-  createTransaction() {
-    this.createTransBtnOnLoad = true;
-    const date = moment(this.transactionForm.get("date")?.value)
-      .startOf("day")
-      .format("YYYY-MM-DDTHH:mm:ss");
-
-    this.transactionForm.patchValue({
-      date: date,
-    });
-
-    this.hrs.request(
-      "post",
-      `transaction/createTransaction`,
-      this.transactionForm.value,
-      async (data: any) => {
-        if (data.success) {
-          this.dialog.open(PopUpModalComponent, {
-            width: "500px",
-            data: {
-              deletebutton: false,
-              title: "Success!",
-              message: "Transaction <b>has been created</b>.",
-            },
-          });
-
-          this.hideMainButton = false;
-          this.viewType = this.viewTypeBefore;
-          this.transactionForm.reset();
-        } else {
-          if (data.message == "Restricted") {
-            this.dialog.open(PopUpModalComponent, {
-              width: "500px",
-              data: {
-                deletebutton: false,
-                title: "Access Denied",
-                message:
-                  "Oops, It looks like you <b>dont have access</b> on this feature.",
-              },
-            });
-          }
-          this.dialog.open(PopUpModalComponent, {
-            width: "500px",
-            data: {
-              deletebutton: false,
-              title: "Server Error",
-              message: data?.error?.message,
-            },
-          });
-        }
-
-        this.createTransBtnOnLoad = false;
-      }
-    );
   }
 
   public generateReportBtnOnLoad = false;
@@ -168,76 +97,5 @@ export class TransactionComponent implements OnInit {
         }
       }
     );
-  }
-
-  cancel() {
-    this.viewType = this.viewTypeBefore;
-    this.tabActiveCashin = false;
-    this.tabActiveCashout = true;
-    this.hideMainButton = false;
-  }
-
-  public currencyStrict(event: any) {
-    let pasteValue = [];
-    const allowedInput = [
-      ".",
-      "0",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-    ];
-    if (event.type == "paste") {
-      pasteValue = event.clipboardData.getData("text/plain").split("");
-      pasteValue.forEach((char: string) => {
-        if (!allowedInput.includes(char)) event.preventDefault();
-      });
-    } else {
-      if (!allowedInput.includes(event.key)) event.preventDefault();
-    }
-  }
-
-  public phoneNumberStrict(event: any) {
-    let pasteValue = [];
-    const allowedInput = [
-      "+",
-      "0",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-    ];
-    if (event.type == "paste") {
-      pasteValue = event.clipboardData.getData("text/plain").split("");
-      pasteValue.forEach((char: string) => {
-        if (!allowedInput.includes(char)) event.preventDefault();
-      });
-    } else {
-      if (!allowedInput.includes(event.key)) event.preventDefault();
-    }
-  }
-
-  public checkFormControlErrors(name: string): boolean {
-    let result = false;
-    if (
-      this.transactionForm.get(name)?.invalid &&
-      this.transactionForm.get(name)?.errors &&
-      (this.transactionForm.get(name)?.dirty ||
-        this.transactionForm.get(name)?.touched)
-    ) {
-      result = true;
-    }
-
-    return result;
   }
 }

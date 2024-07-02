2c0b94a74e196cf2c64dae954cda66ed55417da6
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-07-02 10:57:16 +0800

    update environment to local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   env: "prod",
   SERVER_URL_444: "https://api.gcportal.space:444/api/",
-  SERVER_URL: "https://api.gcportal.space/api/",
-  WEBSOCKET: "https://api.gcportal.space:444",
+  SERVER_URL: "http://localhost/api/",
+  WEBSOCKET: "http://localhost:3001",
 };

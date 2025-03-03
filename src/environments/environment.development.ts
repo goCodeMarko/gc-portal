e64d4248a9439a00b4ae88fb000f07d842ae6fb8
Patrick Marck D <patrickmarckdulaca@gmail.com>
2025-03-03 21:25:28 +0800

    change websocket url to /ws/

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   env: "prod",
   SERVER_URL_444: "https://api.gcportal.space:444/api/",
   SERVER_URL: "https://api.gcportal.space/api/",
-  WEBSOCKET: "https://api.gcportal.space:444",
+  WEBSOCKET: "wss://api.gcportal.space/ws/",
 };
